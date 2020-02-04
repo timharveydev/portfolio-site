610f050a3243b62b4d92303113d0b3e6eee02b26
Tim Harvey <t.harvey1993@gmail.com>
2020-02-04 16:17:50 +0000

    Completed mobile landing page and nav

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Global variables
+const nav = document.getElementById("nav");
+const menu = document.getElementById("nav-menu");
+let toggleState = false;
+
+//Toggles the burger menu on mobile devices
+function toggleMenu() {
+  if (toggleState == false) {
+    menu.style.display = "block";
+    nav.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    toggleState = true;
+  }
+  else {
+    nav.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    menu.style.display = "none";
+    toggleState = false;
+  }
+}
