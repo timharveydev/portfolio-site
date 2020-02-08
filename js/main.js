56ceed5c89391e8f31a1e90fd5cbc4a508d648a7
Tim Harvey <t.harvey1993@gmail.com>
2020-02-08 17:26:45 +0000

    Fixed the bug where nav dissappears after activating the burger menu on small screens and resizing to larger screens

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 //Global variables
-const nav = document.getElementById("nav");
-const menu = document.getElementById("nav-menu");
+const nav = document.getElementById('nav');
+const menu = document.getElementById('nav-menu');
 let toggleState = false;
 
 //Toggles the burger menu on mobile devices
 function toggleMenu() {
   if (toggleState == false) {
-    menu.style.display = "block";
-    nav.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    menu.style.display = 'block';
+    nav.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
     toggleState = true;
   }
   else {
-    nav.style.backgroundColor = "rgba(0, 0, 0, 0)";
-    menu.style.display = "none";
+    nav.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    menu.style.display = 'none';
     toggleState = false;
   }
 }
+
+//Solves issue where nav dissappears after clicking burger menu and resizing window
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1024) {
+    nav.removeAttribute('style');
+    menu.removeAttribute('style');
+    toggleState = false;
+  }
+})
